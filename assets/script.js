dfb43480a973dae0b5060098a5db48c002d7d009
Hshuttle <huntershutle@gmail.com>
2022-09-26 20:03:21 -0400

    wrote correct api URL and created a search tem vaiable

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,18 @@
 var foodFactsURL = "https://world.openfoodfacts.org/api/2";
-var foodFacts2 =
-  "https://world.openfoodfacts.org/cgi/search.pl?action=process&tagtype_0=categories&tag_contains_0=contains&tag_0=fruits";
+// var foodFacts2 =
+//   "https://world.openfoodfacts.org/api/search.pl?action=process&tagtype_0=categories&tag_contains_0=contains&tag_0=fruits";
+var foodFacts2 = `https://us.openfoodfacts.org/cgi/search.pl?search_terms=${searchTerm}&search_simple=1&action=process&tagtype_0=categories&tag_contains_0=contains&tag_0=fruits&json=true`;
 
+var searchTerm = "apple";
 function search() {
-  fetch(foodFacts2, { mode: "no-cors" }).then(function (results) {
-    console.log(results);
-    return results.json().then(function (data) {
+  fetch(foodFacts2)
+    .then(function (results) {
+      console.log(results);
+      return results.json();
+    })
+    .then(function (data) {
       console.log(data);
     });
-  });
 }
+
 $("#button").on("click", search());
