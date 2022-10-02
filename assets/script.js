c4f1fceebf56f667ac3e60649e0c69444ca5cfbc
Hshuttle <huntershutle@gmail.com>
2022-10-02 13:41:31 -0400

    added local storage capabilities

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,7 @@
-function firstSearch(searchTerm) {
+var searchTermArray = JSON.parse(localStorage.getItem("searchTerms")) || [];
+var searchHistory = $("#search-history");
+
+async function firstSearch(searchTerm) {
   var foodFactsURL = `https://us.openfoodfacts.org/cgi/search.pl?search_terms=${searchTerm}&search_simple=1&action=process&tagtype_0=categories&tag_contains_0=contains&tag_0=fruits&json=true`;
   fetch(foodFactsURL)
     .then(function (results) {
@@ -8,50 +11,49 @@ function firstSearch(searchTerm) {
     .then(function (data) {
       console.log(data);
 
-      // Macronutrient Data Elements grams of Carbs(parent); grams of Sugar(child to parent Carbs);  total calories; total fat; grams of protein; Serving Size;
-      var carbs = data.products[0].nutriments.carbohydrates;
-      var carbsUnits = data.products[0].nutriments.carbohydrates_unit;
-      var sugar = data.products[0].nutriments.sugars;
-      var sugarUnits = data.products[0].nutriments.sugars_unit;
-      var fiber = data.products[0].nutriments.fiber;
-      var fiberUnits = data.products[0].nutriments.fiber_unit;
-      var servingSize = data.products[0].serving_size;
-      var cal = data.products[0].nutriments.energy_value;
-      var calUnits = data.products[0].nutriments.energy_unit;
-      var fat = data.products[0].nutriments.fat;
-      var fatUnits = data.products[0].nutriments.fat_unit;
-      var protein = data.products[0].nutriments.proteins;
-      var proteinUnits = data.products[0].nutriments.proteins_unit;
-      var thumbImage = data.products[0].selected_images.front.thumb.en;
-      var productName = data.products[0].product_name_en;
-      var brand = data.products[0].brands_tags[0];
-      var nutritionalLabel = {
-        carbohydrates: carbs + carbsUnits,
-        sugar: sugar + sugarUnits,
-        fiber: fiber + fiberUnits,
-        serving_size: servingSize,
-        calories: cal + calUnits,
-        fat: fat + fatUnits,
-        protein: protein + proteinUnits,
-        product_name: productName,
-        brand: brand,
-        prod_image: thumbImage,
-      };
-      console.log(nutritionalLabel);
-
-      // Additional data element: image of product
-      console.log("Product Image info: " + thumbImage);
-      displayNutritionLabel(nutritionalLabel);
+      if (data.products.length === 0) {
+        console.log("No Food Facts found for " + searchTerm);
+      } else {
+        // Macronutrient Data Elements grams of Carbs(parent); grams of Sugar(child to parent Carbs);  total calories; total fat; grams of protein; Serving Size;
+        var carbs = data.products[0].nutriments.carbohydrates;
+        var carbsUnits = data.products[0].nutriments.carbohydrates_unit;
+        var sugar = data.products[0].nutriments.sugars;
+        var sugarUnits = data.products[0].nutriments.sugars_unit;
+        var fiber = data.products[0].nutriments.fiber;
+        var fiberUnits = data.products[0].nutriments.fiber_unit;
+        var servingSize = data.products[0].serving_size;
+        var cal = data.products[0].nutriments.energy_value;
+        var calUnits = data.products[0].nutriments.energy_unit;
+        var fat = data.products[0].nutriments.fat;
+        var fatUnits = data.products[0].nutriments.fat_unit;
+        var protein = data.products[0].nutriments.proteins;
+        var proteinUnits = data.products[0].nutriments.proteins_unit;
+        var productName = data.products[0].product_name_en;
+        var brand = data.products[0].brands_tags[0];
+        var nutritionalLabel = {
+          carbohydrates: carbs + carbsUnits,
+          sugar: sugar + sugarUnits,
+          fiber: fiber + fiberUnits,
+          serving_size: servingSize,
+          calories: cal + calUnits,
+          fat: fat + fatUnits,
+          protein: protein + proteinUnits,
+          product_name: productName,
+          brand: brand,
+        };
+        console.log(nutritionalLabel);
+        displayNutritionLabel(nutritionalLabel);
+      }
     });
 }
-// TO DO:
-// Setup for loop for the random products pulled in food facts API
-// Review Grading criteria to check for additional functionality
 
-function secondSearch(searchTerm) {
+async function secondSearch(searchTerm) {
+  console.log(searchTerm);
   var fruityViceURL = `https://cors-anywhere.herokuapp.com/https://fruityvice.com/api/fruit/${searchTerm}`;
   console.log(searchTerm);
-  fetch(fruityViceURL)
+  fetch(fruityViceURL, {
+    mode: "cors",
+  })
     .then(function (results) {
       console.log(results);
       return results.json();
@@ -84,11 +86,18 @@ function displayNutritionLabel(nutritionalLabel) {
   $("prod-image").attr("src", nutritionalLabel.prod_image);
 }
 
-function logSearch(searchTerm) {
-  var searchHistory = $("#search-history");
-  var foodButton = $("<button>").attr("id", "history-button");
-  foodButton.text(searchTerm);
-  searchHistory.append(foodButton);
+function logSearch() {
+  console.log(searchTermArray);
+  // var searchHistory = $("#search-history");
+  searchTermArray.forEach(function (searchTerm) {
+    var foodButton = $("<button>").attr("id", "history-button");
+    foodButton.text(searchTerm);
+    foodButton.attr("value", searchTerm);
+    foodButton.click(async function () {
+      firstSearch(this.value).then(secondSearch(this.value));
+    });
+    searchHistory.append(foodButton);
+  });
 }
 
 function displayScience(scientificFacts) {
@@ -96,10 +105,23 @@ function displayScience(scientificFacts) {
   $("#family").text(scientificFacts.family);
   $("#genus").text(scientificFacts.genus);
 }
+
 $("#srch-btn").on("click", function (event) {
   var searchTerm = $("#search-terms").val();
   event.preventDefault();
-  firstSearch(searchTerm);
-  secondSearch(searchTerm);
-  logSearch(searchTerm);
+  if (searchTermArray.indexOf(searchTerm) === -1) {
+    searchTermArray.push(searchTerm);
+    localStorage.setItem("searchTerms", JSON.stringify(searchTermArray));
+    // add button for searched term
+    var foodButton = $("<button>").attr("id", "history-button");
+    foodButton.text(searchTerm);
+    foodButton.attr("value", searchTerm);
+    foodButton.click(async function () {
+      firstSearch(this.value).then(secondSearch(this.value));
+    });
+    searchHistory.append(foodButton);
+  }
+  firstSearch(searchTerm).then(secondSearch(searchTerm));
 });
+
+logSearch();
